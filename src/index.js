95c4381f3c077aecb4ea45d5132e6cc55e15241e
keithowino <designsolutions1629@gmail.com>
2024-04-07 23:56:35 +0300

    Complete lab.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,92 @@
-console.log('%c HI', 'color: firebrick')
+
+// Challenge 1
+let challengeOne = function(){
+
+  const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
+
+  // On page load, fetches the images using the url above.
+  return fetch(imgUrl)
+    .then(response => response.json())
+    .then((data) => {
+      let container = document.getElementById("dog-image-container");
+      let ul = document.createElement("ul");
+
+      let arr = data.message;
+
+      arr.forEach((href) => {
+        let li = document.createElement("li");
+        // Adds image elements to the DOM for each image in the data.message array.
+        li.innerHTML = `<img src="${href}" alt="dog breed image"/>`;
+
+        ul.append(li);
+      });
+
+      container.append(ul);
+    })
+    .catch((error) => {
+      alert(error.message);
+    });
+};
+
+
+// Challenge 2
+let challengeTwo = function(){
+
+  const breedUrl = "https://dog.ceo/api/breeds/list/all";
+
+  // Fetches all the dog breeds
+  return fetch(breedUrl)
+    .then(response => response.json())
+    .then((data) => {
+      let ulContainerTwo = document.getElementById("dog-breeds");
+      let selectElement = document.getElementById('breed-dropdown');
+      let obj = data.message;
+
+      let selectedValue;
+
+      selectElement.addEventListener("change", (e) => {
+        selectedValue = e.target.value;
+        ulContainerTwo.innerHTML = ''; // Clear existing list
+
+        if (selectedValue) {
+          for (let key in obj) {
+            if (key.slice(0, 1) === selectedValue) {
+              let li = document.createElement("li");
+              li.textContent = key;
+              ulContainerTwo.appendChild(li);
+
+              li.addEventListener("click", (e) => {
+                e.target.style.color = "#0ff";
+              })
+            }
+          }
+        } else {
+          for (let key in obj) {
+            let li = document.createElement("li");
+            li.textContent = key;
+            ulContainerTwo.appendChild(li);
+
+            li.addEventListener("click", (e) => {
+              e.target.style.color = "#0ff";
+            })
+          }
+        }
+      });
+
+
+
+
+    })
+    .catch((error) => {
+      alert(error.message);
+    });
+};
+
+
+
+let interaction = function(){
+  challengeOne();
+  challengeTwo();
+};
+
+document.addEventListener("DOMContentLoaded", interaction);
